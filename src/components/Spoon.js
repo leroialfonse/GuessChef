2c8ae450f06877544601fec3e23ed337ceb4c1cc
Brandon White <brandonwhitedev@gmail.com>
2023-12-08 03:10:49 -0500

    Writing out the fetch to grab recipies from the ingredient search form.

--- src/components/Spoon.js
+++ src/components/Spoon.js
@@ -1,128 +1,44 @@
-// import React, { useState, useEffect } from 'react';
-
-// export default function Spoon() {
-
-//     const IngForm = ({ onSearch }) => { }
-
-//     // const [data, setData] = React.useState('')
-
-//     // const fetchInfo = (e) => {
-//     //     e.preventDefault();
-
-//     //     return fetch(url)
-//     //         .then((res) => res.json())
-//     //         .then((d) => setData(d))
-//     // }
-
-//     // useEffect(() => {
-//     //     fetchInfo();
-//     // }, [])
-
-
-//     < div className="info" >
-
-//              const handleSubmit = async (e) => {
-//             // Stop default behaviors..
-
-//             e.preventDefault()
-
-//         const response = await fetch('https://api.spoonacular.com/recipes/findByIngredients?ingredients', {
-//             method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-
-//             },
-//         body: JSON.stringify({ingredients}),
-//         })
-
-
-//         const data = await response.json()
-
-//         onLogin(data.ingredients)
-//     }
-
-
-
-//         return (
-//         <form onSubmit={handleSubmit}>
-//             <input type='text'
-//                 placeholder='Username'
-//                 value={username}
-//                 onChange={(e) => setUserName(e.target.value)}
-
-//             />
-//             <input type='password'
-//                 placeholder='Password'
-//                 value={password}
-//                 onChange={(e) => setPassword(e.target.value)}
-
-//             />
-
-//             <button type='submit'>Login</button>
-//         </form>
-
-//     </div >
-//     )
-// }
-
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Spoon = () => {
 
-    const [recipe, setRecipe] = useState({})
-    const [ingredient, setIngredient] = useState('')
+    const [formData, setFormData] = React.useState({
+        ingredient: ''
+    })
+    // console.log(formData)
 
-    // const [password, setPassword] = useState('')
+    React.useEffect(() => {
+        fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=350ed6a1f4c84c85ba247a1161be2077&ingredients=$&number=3`)
+    }, [])
 
-    // What to do when the login form button is clicked.
-
-    const handleSubmit = async (e) => {
-        // Stop default behaviors..
-
-        e.preventDefault()
-
-        const response = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=350ed6a1f4c84c85ba247a1161be2077&ingredients=${ingredient}&number=3`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-
-            },
-            // body: JSON.stringify({ ingredient }),
+    function handleChange(event) {
+        // const [name, value, type] = event.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
         })
-
-
-        const data = await response.json()
-
-        console.log(data)
-
     }
 
-
-
+    function handleSubmit(event) {
+        event.prevent.default()
+        fetch()
+    }
     return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <input type='text'
-                    placeholder='ingredient'
-                    value={ingredient}
-                    onChange={(e) => setIngredient(e.target.value)}
-
-                />
-                {/* <input type='password'
-                placeholder='Password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-
-            /> */}
-
-                <button type='submit'>search</button>
-
-            </form>
-            <div>{data}</div>
-        </>
+        <form onSubmit={handleSubmit}>
+            <input
+                type='text'
+                placeholder='ingredient?'
+                onChange={handleChange}
+                name='ingredient'
+                value={formData.ingredient}
+            />
+            <button>Search for recipes!</button>
+        </form>
     )
 }
+
 
 
 
