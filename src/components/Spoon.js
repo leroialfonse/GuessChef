f8638428e3e5e29d38fa7e8d68adf1cdff9036c7
Brandon White <brandonwhitedev@gmail.com>
2023-12-11 14:42:00 -0500

    Still picking through the api, trying to return the info I want. Ran out of spoonacular requests for the day. SMH. Consider some styling and adding the effects  I want to see.

--- src/components/Spoon.js
+++ src/components/Spoon.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const Spoon = () => {
 
@@ -15,9 +17,15 @@ const Spoon = () => {
             // fetch('https://api.spoonacular.com/recipes/random?apiKey=350ed6a1f4c84c85ba247a1161be2077')
 
             // a rigormortonson api key
-            fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=ad25a893b45f4e808dc312fa5cf225fa&query=${userInput}`)
+            fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=ad25a893b45f4e808dc312fa5cf225fa&addRecipeInformation=true&instructionsRequired=true&query=${userInput}`)
                 .then(res => res.json())
-                .then(data => setRecipeData(data.results.map(recipe => recipe.title)))
+                // .then(data => setRecipeData(data.results.map(recipe => recipe.title)))
+                .then(data => {
+                    console.log(data)
+                    setRecipeData(data.results.map(recipe => [recipe.title, recipe.analyzedInstructions]))
+                })
+            // .then(data => setRecipeData(data.results.map(recipe => 
+
         }
     }, [userInput])
 
@@ -46,10 +54,25 @@ const Spoon = () => {
                 />
                 <button>Search for recipes!</button>
             </form>
-
-            {recipeData.map((title, index) => (<p key={index}>{title}</p>))}
+            {recipeData.map((title, results, analyzedInstructions, id) => (<p key={id}>{title} {analyzedInstructions}</p>))}
+            {/* {recipeData.map((title, index) => (<p key={index}>{title}</p>))}
+            <>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title><p>{setRecipeData.title}</p></Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            </>
+        </> */}
         </>
     )
+
 }
 
 
