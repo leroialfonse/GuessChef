707d484cd92e632b944842e6e4e629594039de79
Brandon White <brandonwhitedev@gmail.com>
2023-12-12 23:57:33 -0500

    Working out that useEffect. Work on some styles now.

--- src/components/Spoon.js
+++ src/components/Spoon.js
@@ -4,71 +4,38 @@ import Button from 'react-bootstrap/Button';
 
 const Spoon = () => {
 
-    const [recipeData, setRecipeData] = React.useState()
+    const [recipeData, setRecipeData] = React.useState([])
     const [userInput, setUserInput] = React.useState('')
 
 
+
     React.useEffect(() => {
-        fetch(`https://api.nasa.gov/planetary/apod?api_key=xdrHkDW0PHo4j1wUO4jcxv4qYT13amvEHGs4qvB7&date=${userInput}`)
+
+        // all brandonwhitedev api keys...
+        fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=350ed6a1f4c84c85ba247a1161be2077&number=3&instructionsRequired?true&ingredients=${userInput}`)
+            // fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=350ed6a1f4c84c85ba247a1161be2077&query=${userInput}`)
+            // fetch('https://api.spoonacular.com/recipes/random?apiKey=350ed6a1f4c84c85ba247a1161be2077&query=${userInput}')
+
+            // a rigormortonson api key
+            // fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=ad25a893b45f4e808dc312fa5cf225fa&addRecipeInformation=true&instructionsRequired=true&query=${userInput}`)
             .then(res => res.json())
-            .then(data => {
-
-                setRecipeData(data)
+            .then((data) => {
                 console.log(data)
-            }
-            )
-    }, [])
+                setRecipeData(data.results.map((image, title) => (
+                    <>
+                        <p>{data.title}</p>
+                        <img src={data.image} alt='Recipe Stuff.' />
+                    </>
+                )))
 
-    // React.useEffect(() => {
+            })
 
-    // all brandonwhitedev api keys...
-    // fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=350ed6a1f4c84c85ba247a1161be2077&ingredients=${userInput}`)
-    // fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=350ed6a1f4c84c85ba247a1161be2077&query=${userInput}`)
-    // fetch('https://api.spoonacular.com/recipes/random?apiKey=350ed6a1f4c84c85ba247a1161be2077')
-
-    // a rigormortonson api key
-    // fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=ad25a893b45f4e808dc312fa5cf225fa&addRecipeInformation=true&instructionsRequired=true&query=${userInput}`)
-    //     .then(res => res.json())
-    //     // .then(data => setRecipeData(data.results.map(recipe => recipe.title)))
-    //     .then(data => {
-    //         console.log(data)
-    //         setRecipeData(data.results.map(recipe => [recipe.title, recipe.analyzedInstructions]))
-    //     })
-    // .then(data => setRecipeData(data.results.map(recipe => 
+    }, [userInput])
 
 
 
-    // Practicing with API with swapi...
-    //         fetch(`http://swapi.dev/api/people/?search=${userInput}`)
-    //             .then(res => res.json())
-    //             .then(data => {
-    //                 console.log(data.results.map(data => ))
-
-    //             })
-    //             .catch(error => {
-    //                 console.log(`error ${error}`)
-    //             })
-    //     }
-
-    // }, [userInput])
-
-    // Trying to practice Api FETCH with nasa APOD.
-    //     fetchPosts = async () => {
-    //         const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=xdrHkDW0PHo4j1wUO4jcxv4qYT13amvEHGs4qvB7&date=${userInput}`)
-    //         const recipeData = await response.json();
 
 
-    //     };
-    // }, [userInput]
-    // );
-
-    // function getPhoto() {
-    //     const data = recipeData.length
-    //     setRecipeData(prevData => ({
-    //         ...prevData,
-    //         data.hdurl
-    //     }))
-    // }
 
     function handleChange(event) {
         setUserInput(event.target.value)
@@ -80,24 +47,6 @@ const Spoon = () => {
 
     function handleSubmit(event) {
         event.preventDefault()
-        // let recipesInfo = setRecipeData.map(results => results)
-        // let starWar = setRecipeData.map()
-
-        // return (
-        //     <>
-        //         <Card style={{ width: '18rem' }}>
-        //             <Card.Img variant="top" src="holder.js/100px180" />
-        //             <Card.Body>
-        //                 <Card.Title><p>{setRecipeData.title}</p></Card.Title>
-        //                 <Card.Text>
-        //                     Some quick example text to build on the card title and make up the
-        //                     bulk of the card's content.
-        //                 </Card.Text>
-        //                 <Button variant="primary">Go somewhere</Button>
-        //             </Card.Body>
-        //         </Card>
-        //     </>
-        // )
     }
 
 
@@ -105,7 +54,7 @@ const Spoon = () => {
         <>
             <form onSubmit={handleSubmit}>
                 <input
-                    type='date'
+                    type='text'
                     // placeholder='ingredient?'
                     onChange={handleChange}
                     // name='ingredient'
@@ -114,12 +63,19 @@ const Spoon = () => {
                 />
                 <button>Search for recipes!</button>
             </form>
-            {/* <h3>{recipeData.explanation}</h3> */}
 
-            <p>{recipeData.data.map((info) => {
-                <p>{info.explanation}</p>
-            })}</p>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={recipeData.image} alt='Recipe Image' style={{ height: '18rem', width: '18rem' }} />
+                <Card.Body>
+                    <Card.Title><p>{recipeData.title}</p></Card.Title>
+                    <Card.Text>
+                        {recipeData.explanation}
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
         </>
+
     )
 
 }
