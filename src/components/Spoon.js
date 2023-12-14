8ae5651ba9475e07b6caaad034c6cf95b396816b
Brandon White <brandonwhitedev@gmail.com>
2023-12-14 18:21:17 -0500

    Got the searc working, will pull up recipes. now figure out how to get the instuctions and show them on the page. Consider what to do for styles. Maybe a food item carousel until user supplies input???

--- src/components/Spoon.js
+++ src/components/Spoon.js
@@ -11,23 +11,34 @@ const Spoon = () => {
 
     React.useEffect(() => {
 
-        // all brandonwhitedev api keys...
-        fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=350ed6a1f4c84c85ba247a1161be2077&number=3&instructionsRequired?true&ingredients=${userInput}`)
-            // fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=350ed6a1f4c84c85ba247a1161be2077&query=${userInput}`)
-            // fetch('https://api.spoonacular.com/recipes/random?apiKey=350ed6a1f4c84c85ba247a1161be2077&query=${userInput}')
+        // brandonkwhite3@gmail.com
+        // 8150e43b22914ff0ab67ee2af6bb4338
 
-            // a rigormortonson api key
+        // all brandonwhitedev api keys...
+        // Original COMPLEX SEARCH API call
+        // fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=350ed6a1f4c84c85ba247a1161be2077&number=3&instructionsRequired=true&ingredients=${userInput}`)
+        // MODIFIED COMPLEX, to look for the recipe instructions.
+        // fetch(`https://api.spoonacular.com/recipes/complexSearch?addRecipeInformation=true&number=3&ingredients=${userInput}&apiKey=350ed6a1f4c84c85ba247a1161be2077`)
+        // fetch(`https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=350ed6a1f4c84c85ba247a1161be2077`)
+
+        // fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=350ed6a1f4c84c85ba247a1161be2077&query=${userInput}`)
+        // fetch('https://api.spoonacular.com/recipes/random?apiKey=350ed6a1f4c84c85ba247a1161be2077&query=${userInput}')
+
+        // a rigormortonson api key
+        // fetch(`https://api.spoonacular.com/recipes/complexSearch?query=ingredients=${userInput}&addRecipeInformation=true&number=1&apiKey=ad25a893b45f4e808dc312fa5cf225fa`)
+
+        // brandonkwhite3 key....
+        fetch(`https://api.spoonacular.com/recipes/complexSearch?query=ingredients=${userInput}&addRecipeInformation=true&number=3&apiKey=8150e43b22914ff0ab67ee2af6bb4338`)
+
             // fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=ad25a893b45f4e808dc312fa5cf225fa&addRecipeInformation=true&instructionsRequired=true&query=${userInput}`)
             .then(res => res.json())
             .then((data) => {
-                console.log(data)
-                setRecipeData(data.results.map((image, title) => (
-                    <>
-                        <p>{data.title}</p>
-                        <img src={data.image} alt='Recipe Stuff.' />
-                    </>
-                )))
-
+                console.log(data.results)
+                setRecipeData(data)
+            })
+            .catch(error => {
+                console.error('Error with fetching the data', error);
+                setRecipeData([])
             })
 
     }, [userInput])
@@ -40,40 +51,56 @@ const Spoon = () => {
     function handleChange(event) {
         setUserInput(event.target.value)
 
+
     }
+
+
+
+    // function getNewRecipes() {
+
+    // }
 
 
 
 
     function handleSubmit(event) {
         event.preventDefault()
+        // const url = recipeData[userInput]
+        // setRecipeData(prevRecipe => ({
+        //     ...prevRecipe,
+        //     newRecipe: url
+        // }))
+
     }
 
 
     return (
+
         <>
             <form onSubmit={handleSubmit}>
                 <input
                     type='text'
-                    // placeholder='ingredient?'
+                    placeholder='sugar, cinnamon, lemon?'
                     onChange={handleChange}
-                    // name='ingredient'
-                    name='date'
+                    name='ingredient'
                     value={userInput}
                 />
                 <button>Search for recipes!</button>
             </form>
 
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={recipeData.image} alt='Recipe Image' style={{ height: '18rem', width: '18rem' }} />
-                <Card.Body>
-                    <Card.Title><p>{recipeData.title}</p></Card.Title>
-                    <Card.Text>
-                        {recipeData.explanation}
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
+            {/* <pre>{JSON.stringify(recipeData, null, 1)}</pre> */}
+            {userInput ? recipeData.results.map(info =>
+                <Card key={info.id} style={{ width: '18rem', border: '2px solid gray', margin: '2.5rem', display: 'flex', borderRadius: '8px' }}>
+                    <Card.Img variant="top" src={info.image} alt='Recipe Image' style={{ height: '10rem', width: '10rem' }} />
+                    <Card.Body>
+                        <Card.Title><p>{info.title}</p></Card.Title>
+                        <Card.Text>
+                            <p>{info.analyzedItructions}</p>
+                        </Card.Text>
+                        <Button variant="primary">Let's Make It!</Button>
+                    </Card.Body>
+                </Card>
+            ) : "Type in some ingredients to get started!"}
         </>
 
     )
