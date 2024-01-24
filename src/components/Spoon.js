1af05f3304eee790ba8f020ca487331b4b55faa1
Brandon White <brandonwhitedev@gmail.com>
2024-01-24 02:11:42 -0500

    Removed the link to the recipe info... Work on figuring out if I want to use ReactQuery/TanStack Query. Or if I'm going to go with links to the recipes. And how to get that information displayed.

--- src/components/Spoon.js
+++ src/components/Spoon.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+// import { Link } from 'react-router-dom';
+
+
+
+
 
 const Spoon = () => {
+
 
     const [recipeData, setRecipeData] = React.useState([])
     const [userInput, setUserInput] = React.useState('')
@@ -54,15 +60,6 @@ const Spoon = () => {
 
     }
 
-
-
-    // function getNewRecipes() {
-
-    // }
-
-
-
-
     function handleSubmit(event) {
         event.preventDefault()
         // const url = recipeData[userInput]
@@ -80,7 +77,7 @@ const Spoon = () => {
             <form onSubmit={handleSubmit}>
                 <input
                     type='text'
-                    placeholder='sugar, cinnamon, lemon?'
+                    placeholder='sugar, cinnamon, nutmeg?'
                     onChange={handleChange}
                     name='ingredient'
                     value={userInput}
@@ -97,9 +94,10 @@ const Spoon = () => {
                         <Card.Text>
                             <p>{info.analyzedItructions}</p>
                         </Card.Text>
-                        <Button variant="primary">Let's Make It!</Button>
+                        {/* <Link to={`https://api.spoonacular.com/recipes/${info.id}/information?apiKey=8150e43b22914ff0ab67ee2af6bb4338`}>Let's Make It</Link> */}
+                        <Button variant="primary"  >Let's Make It!</Button>
                     </Card.Body>
-                </Card>
+                </Card >
             ) : "Type in some ingredients to get started!"}
         </>
 
@@ -107,59 +105,4 @@ const Spoon = () => {
 
 }
 
-
-// ? This still isn't working, so I need to review how to use this API. I think I have it right, but I'm stilll.... calling to map an object? Not adressing the json data correctly?
-
-
 export default Spoon;
-
-// import React from 'react';
-
-// const Spoon = () => {
-//     const [recipeData, setRecipeData] = React.useState([]);
-//     const [userInput, setUserInput] = React.useState('');
-
-//     React.useEffect(() => {
-//         if (userInput.trim() !== '') {
-//             fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=350ed6a1f4c84c85ba247a1161be2077&query=${userInput}`)
-//                 .then(res => res.json())
-//                 .then(data => {
-//                     // Assuming data.results is an array of recipes
-//                     // Update state with an array of titles
-//                     setRecipeData(data.results.map(recipe => recipe.title));
-//                 })
-//                 .catch(error => console.error('Error fetching data:', error));
-//         }
-//     }, [userInput]);
-
-//     function handleChange(event) {
-//         setUserInput(event.target.value);
-//     }
-
-//     function handleSubmit(event) {
-//         event.preventDefault();
-//         // No need to log recipeData here, as it will be updated by the useEffect
-//     }
-
-//     return (
-//         <>
-//             <form onSubmit={handleSubmit}>
-//                 <input
-//                     type='text'
-//                     placeholder='ingredient?'
-//                     onChange={handleChange}
-//                     name='ingredient'
-//                     value={userInput}
-//                 />
-//                 <button>Search for recipes!</button>
-//             </form>
-
-//             {/* Display each recipe title in a separate paragraph */}
-//             {recipeData.map((title, index) => (
-//                 <p key={index}>{title}</p>
-//             ))}
-//         </>
-//     );
-// };
-
-// export default Spoon;
