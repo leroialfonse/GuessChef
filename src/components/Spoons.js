ad07790e2239d340240517a2f66481349bd76950
Brandon White <brandonwhitedev@gmail.com>
2024-01-27 18:29:43 -0500

    Need to query the API and return result

--- src/components/Spoons.js
+++ src/components/Spoons.js
@@ -6,22 +6,34 @@ const RECIPES = [
     { id: 2, title: "Here's another thing" }
 ]
 
+// const userInput = document.querySelector(".userInputItem").textContent
 
 function Spoons() {
     const recipesQuery = useQuery({
-        queryKey: ["posts"],
-        queryFn: () => wait(1000).then(() => fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=ad25a893b45f4e808dc312fa5cf225fa&addRecipeInformation=true&instructionsRequired=true&query`)
-            .then(res => res.json()))
+        queryKey: ["recipes"],
+        //     queryFn: () => wait(1000).then(() => fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=ad25a893b45f4e808dc312fa5cf225fa&addRecipeInformation=true&instructionsRequired=true&query=${userInput}`)
+        //         .then(res => res.json()))
+        // })
+
+        queryFn: () => wait(1000).then(() => [...RECIPES])
     })
+
 
     if (recipesQuery.isLoading) return <h1>Loadin...</h1>
     if (recipesQuery.isError) {
         return <pre>{JSON.stringify(recipesQuery.error)}</pre>
     }
     return (
-        <h1>TanStack Run.</h1>
+        <>
+            <h1>TanStack Run.</h1>
 
-
+            <div> {recipesQuery.data.map(recipe => (
+                <div key={recipe.id}>{recipe.title}</div>))}
+            </div>
+            {/* <form>
+                <input class="userInputItem" type='text' ></input>
+            </form> */}
+        </>
     )
 
 
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <QueryClientProvider client={queryClient} >
     <App />
-    <ReactQueryDevtools initialIsOpen={false} />
+    <ReactQueryDevtools />
   </QueryClientProvider>
 
 );
