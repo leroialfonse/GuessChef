f39c2bedac37c6c7ba2977583ed093149e904c52
Brandon White <brandonwhitedev@gmail.com>
2024-04-06 00:43:19 -0400

    Changed the ignored files for front and back end

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import './index.css'
+import React, { useState, useEffect } from 'react';
+import './index.css';
 
-import Spoon from './components/Spoon';
+// import Spoon from './components/Spoon';
+
 
 
 
 
 
 function App() {
+  const [message, setMessage] = React.useState('');
+
+  React.useEffect(() => {
+    fetch("http://localhost:8080/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+  }, []);
 
 
   return (
     <>
 
-      <Spoon />
+      {/* <Spoon /> */}
+      <div>
+        <h1 >{message}</h1>
+      </div>
 
     </>
   )
