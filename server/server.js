efe53749271f6df3d446910bbd2047cc4273f15b
Brandon White <brandonwhitedev@gmail.com>
2024-04-06 00:25:02 -0400

    Server set up

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/message', (req, res) => {
+    res.json({ message: "hello from me!" })
+})
+
+app.listen(8080, () => {
+    console.log(`Server is running on port 8080.`)
+});
